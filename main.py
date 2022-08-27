f09eb0a738b3df1aa3babc4ec0ecd4778af616f8
Eldor900 <70474186+Eldor900@users.noreply.github.com>
2022-08-27 13:27:05 +0500

    Update main.py

--- main.py
+++ main.py
@@ -535,4 +535,21 @@ def views(message):
      bot.send_photo(chatId, ve13)
      bot.send_photo(chatId, ve14)
 
-bot.polling()
+
+@server.route('/' + API_KEY, methods=['POST'])
+def get_message():
+    json_string = request.get_data().decode('utf-8')
+    update = telebot.types.Update.de_json(json_string)
+    bot.process_new_updates([update])
+    return '!', 200
+
+
+@server.route('/')
+def webhook():
+    bot.remove_webhook()
+    bot.set_webhook(url=APP_URL)
+    return '!', 200
+
+
+if __name__ == '__main__':
+    server.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
